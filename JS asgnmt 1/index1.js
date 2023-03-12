75fc57c480e515cf54937b77fca3a91b77345c2d
Sharmeentariq90 <122076590+Sharmeentariq90@users.noreply.github.com>
2023-03-12 12:04:37 -0700

    Add files via upload

---
+++ JS asgnmt 1/index1.js
@@ -0,0 +1,20 @@
+// cHAPTER #1
+// var name="hello";
+// alert(name);
+// Q no:1
+var userName = "Dear";
+alert("Welcome " + userName + "!");
+// Q no:2
+// alert("plz enter a valid password!");
+var massage="PLz enter a valid password";
+alert(massage);
+// Q no:3
+alert("Welcome to JS Land...\n Happy Coding!");
+// Q no:4
+var massage1="Welcome to JS Land...";
+var massage2="Happy Coding!";
+alert(massage1);
+alert(massage2);
+// Q no:5
+var massage3 = ("Hello...I can run JS through my web browser's console");
+console.log(massage3);
---
+++ JS asgnmt 1/index4.js
@@ -0,0 +1,20 @@
+
+// Q no:1
+var x="hello";
+var y="Whole";
+var z="world"
+// Q no:2:
+// legal variable:
+// 1._my variables
+// 2.this_is_valid
+// 3.a123
+// 4.$money
+// 5._underscore
+// lillegal variable:
+// 1.123illrgal
+// 2.var
+// 3.function
+// 4.ture
+// 5.null
+// Q no:3
+document.write("3. Display this in your browser");
